1ffa6413ee518626dbb755eed45d3a4b8bdba1e2
Giuliano Espejo Mezzabotta <129322090+Giuliano-Espejo@users.noreply.github.com>
2024-10-13 16:22:48 -0300

    update description

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -8,11 +8,14 @@ export default function Presentation() {
                 <div className="text-content-presentation">
                     <h1>Giuliano Espejo</h1>
                     <h2>Back-End Developer</h2>
-                </div> 
+                </div>
                 <img src="/static/fotocara.png" alt="foto cara" className="foto-cara" />
             </div>
             <p className="mx-4">
-               Soy Giuliano Espejo, Técnico Universitario en Programación graduado de la Universidad Tecnológica Nacional regional Mendoza. Me desenvuelvo como desarrollador Back-End utilizando Java-Spring, busco construir soluciones robustas y escalables que impulsan aplicaciones de alto rendimiento.  
+                ¡Hola! Soy <strong>Giuliano Espejo</strong>, un Desarrollador Back-End, con conocimientos Full Stack y con una sólida formación en <strong>Java</strong> y más de un año de experiencia en el desarrollo
+                 de aplicaciones backend y frontend. Me apasiona crear soluciones eficientes y escalables, siempre utilizando prácticas de <strong>código 
+                 limpio</strong> y aplicando arquitectura limpia en mis proyectos. He trabajado en diversos proyectos, desde sistemas de microservicios hasta
+                  gestión de stock y empleados, y tengo un enfoque centrado en la mejora continua y la innovación.
             </p>
             <div className="cv">
                 <a href="/static/GiulianoEspejoCV.pdf" target="_blank">
